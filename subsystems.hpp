d0d33516acf9ebdf6f4744caa857b8fbdb1d44b0
YoshiKing422 <yashvardhansoni12@gmail.com>
2026-09-13 15:37:14 -0400

    Add files via upload

---
+++ subsystems.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+extern pros::Motor cascade_left;
+extern pros::Motor cascade_right;
+
+extern pros::Motor intake; 
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
